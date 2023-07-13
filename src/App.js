eb1db0927ca4af9a7427c42a91fad47589ad6e3d
Exilic <tobias.engberg@exilic.se>
2023-07-13 15:38:23 +0200

    Simple translate to React

--- src/App.js
+++ src/App.js
@@ -1,33 +1,70 @@
 import './App.css';
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
-  const handleKey = (e) => {
-    e.preventDefault();
-    switch (e.key) {
-      case 'ArrowLeft':
-        swipeLeft();
-        break;
-      case 'ArrowUp':
-        swipeUp();
-        break;
-      case 'ArrowRight':
-        swipeRight();
-        break;
-      case 'ArrowDown':
-        swipeDown();
-        break;
-      default:
-        break;
-    }
-  };
+
+  const [gameNumbers, setGameNumbers] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+  const [feedback, setFeedback] = useState("The present game is unsolved");
+  const [newGame, setNewGame] = useState(true);
 
   useEffect(() => {
+    if (newGame) {
+      do {
+        let newGameNumbers = [...gameNumbers];
+        for (let i = gameNumbers.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [newGameNumbers[i], newGameNumbers[j]] = [newGameNumbers[j], newGameNumbers[i]];
+        }
+        setGameNumbers(newGameNumbers);
+      } while (isSolvable() === false)
+      setNewGame(false);
+      setFeedback(() => "The present game is unsolved");
+    }
+  }, [gameNumbers, isSolvable, newGame]);
+
+  useEffect(() => {
+    const handleKey = (e) => {
+      e.preventDefault();
+      let positionNumber;
+      switch (e.key) {
+        case 'ArrowLeft':
+          if (((gameNumbers.indexOf(16) + 1) % 4) !== 0) {
+            positionNumber = 1;
+          }
+          break;
+        case 'ArrowUp':
+          if(gameNumbers.indexOf(16) < 12){
+            positionNumber = 4;
+          }
+          break;
+        case 'ArrowRight':
+          if (((gameNumbers.indexOf(16) + 4) % 4) !== 0){
+            positionNumber = -1;
+          }
+          break;
+        case 'ArrowDown':
+          if(gameNumbers.indexOf(16) > 3){
+            positionNumber = -4;
+          }
+          break;
+        default:
+          break;
+      }
+      let newGameNumbers = [...gameNumbers];
+      newGameNumbers[gameNumbers.indexOf(16)] = newGameNumbers[(gameNumbers.indexOf(16) + positionNumber)];
+      newGameNumbers[(gameNumbers.indexOf(16) + positionNumber)] = 16;
+      setGameNumbers(() => newGameNumbers);
+      if (gameIsWon()) {
+        setFeedback(() => "You have won!");
+      }
+    };
     window.addEventListener("keydown", handleKey);
     return () => window.removeEventListener("keydown", handleKey)
-  }, []);
+  }, [gameNumbers]);
 
-  const isGameWon = () => {
+
+
+  const gameIsWon = () => {
     for (let i = 0; i < 16; i++) {
       if (gameNumbers[i] !== (i+1)) {
         return false;
@@ -35,46 +72,9 @@ function App() {
     } return true;
   };
 
-  let numberSixteen = 15;
-
-  let gameNumbers = new Array(16);
-  const startNewGame = () => {
-    for (let index = 0; index < 16; index++) {
-      gameNumbers[index] = (index+1);
-    }
-    do {
-      newRandomOrder();
-      determineSixteen();
-    } while (isSolvable() === false)
-
-    rearrangeCSS();
-    document.getElementById('result').innerHTML = "The present game is unsolved";
-  }
-
-  function newRandomOrder() {
-    for (let i = gameNumbers.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [gameNumbers[i], gameNumbers[j]] = [gameNumbers[j], gameNumbers[i]];
-    }
-  }
-
-  function rearrangeCSS() {
-    for (let i = 0; i < 16; i++) {
-      let c = gameNumbers[i];
-      setRow(c, (1+Math.floor(i/4)));
-      setColumn(c, ((i+1)-(Math.floor(i/4)*4)));
-    }
-  }
-
-  function determineSixteen() {
-    for (let i = 0; i < 16; i++) {
-      if (gameNumbers[i] === 16)
-        numberSixteen = i;
-    }
-  }
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   function isSolvable() {
-    const isEven = Math.floor((numberSixteen) / 4 ) % 2 === 0;
+    const isEven = Math.floor(gameNumbers.indexOf(16) / 4 ) % 2 === 0;
     let count = 0;
     for (let i = 0; i < 15; i++) {
       if(gameNumbers[i] !== 16) {
@@ -87,84 +87,19 @@ function App() {
     return !isEven && count % 2 === 0;
   }
 
-  function swipeLeft() {
-    if (((numberSixteen+1) % 4) !== 0){
-      let a = ((numberSixteen+1)-(Math.floor(numberSixteen/4)*4));
-      setColumn(16,(a+1));
-      setColumn(gameNumbers[(numberSixteen+1)], a);
-      finishSwipe(1);
-    }else{}
-  }
-
-  function swipeRight() {
-    if (((numberSixteen+4) % 4) !== 0){
-      let a = ((numberSixteen+1)-(Math.floor(numberSixteen/4)*4));
-      setColumn(16,(a-1));
-      setColumn(gameNumbers[(numberSixteen-1)], a);
-      finishSwipe(-1);
-    }else{}
-  }
-
-  function swipeDown() {
-    if(numberSixteen>3){
-      setRow(16, (Math.floor(numberSixteen/4)));
-      setRow(gameNumbers[(numberSixteen-4)], ((Math.floor(numberSixteen/4))+1));
-      finishSwipe(-4);
-    }else{}
-  }
-
-  function swipeUp() {
-    if(numberSixteen<12){
-      setRow(16, ((Math.floor(numberSixteen/4)+2)));
-      setRow(gameNumbers[(numberSixteen+4)], (Math.floor(numberSixteen/4)+1));
-      finishSwipe(4);
-    }else{}
-  }
-
-  function finishSwipe(positionNumber) {
-    swapNumbers(numberSixteen, positionNumber);
-    numberSixteen = (numberSixteen + positionNumber);
-    if (isGameWon()) document.getElementById('result').innerHTML = "You have won!";
-  }
-
-  function setRow(tile, row) {
-    document.getElementById('tile'+tile).style.gridRow = row;
-  }
-
-  function setColumn(tile, column) {
-    document.getElementById('tile'+tile).style.gridColumn = column;
-  }
-
-  function swapNumbers(sixteen, other) {
-    gameNumbers[sixteen] = gameNumbers[(sixteen + other)];
-    gameNumbers[(sixteen + other)] = 16;
-  }
-
   return (
     <>
       <h1>Order numbers from 1 to 15 by moving numbers into the free spot with
         the arrow keys</h1>
       <div id="gamePlayground">
-        <div id="tile1">1</div>
-        <div id="tile2">2</div>
-        <div id="tile3">3</div>
-        <div id="tile4">4</div>
-        <div id="tile5">5</div>
-        <div id="tile6">6</div>
-        <div id="tile7">7</div>
-        <div id="tile8">8</div>
-        <div id="tile9">9</div>
-        <div id="tile10">10</div>
-        <div id="tile11">11</div>
-        <div id="tile12">12</div>
-        <div id="tile13">13</div>
-        <div id="tile14">14</div>
-        <div id="tile15">15</div>
-        <div id="tile16"></div>
+        {gameNumbers.map(gameNumber => (
+          gameNumber === 16 ? <div></div> : <div>{gameNumber}</div>
+        ))};
+
       </div>
       <div id="info">
-        <button onClick={startNewGame}>New Game</button>
-        <div id="result">The present game is over</div>
+        <button onClick={() => setNewGame(true)}>New Game</button>
+        <div id="result">{feedback}</div>
       </div>
     </>
   );
